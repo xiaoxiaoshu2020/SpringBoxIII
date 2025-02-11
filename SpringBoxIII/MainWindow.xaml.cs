f132ad301b3af1d3faf79d034caab592f182bbc7
xiaoxiaoshu2020 <xiaoxiaoshu2020@outlook.com>
2025-02-11 11:32:26 +0800

    添加了疑似有BUG的事件控制器，和行为“耗子拖走鼠标”，将播放移动动画拆分成函数

--- SpringBoxIII/MainWindow.xaml.cs
+++ SpringBoxIII/MainWindow.xaml.cs
@@ -35,11 +35,18 @@ namespace SpringBoxIII
         private DispatcherTimer _timer;
 
         //获取鼠标位置
-        //[System.Runtime.InteropServices.DllImport("user32.dll")]
-        //public static extern bool GetCursorPos(out System.Drawing.Point lpPoint);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out System.Drawing.Point lpPoint);
+
+        //改变鼠标位置
+        [DllImport("user32.dll")]
+        public static extern bool SetCursorPos(int x, int y);
 
         private bool _isAnimationCompleted = true;
-        private int _moveSpeed = 1000;
+        private bool _isMovedToCursor = false;
+        private bool _isEventCompleted = true;
+        private int _moveSpeed = 350;
+        private int randomEvent = 0;
         //private Point _point = new Point(0, 0);
 
 
@@ -57,8 +64,10 @@ namespace SpringBoxIII
             };
 
             // 初始化定时器
-            _timer = new DispatcherTimer();
-            _timer.Interval = TimeSpan.FromMilliseconds(10);
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(10)
+            };
             _timer.Tick += Timer_Tick;
             _timer.Start();
 
@@ -80,7 +89,7 @@ namespace SpringBoxIII
             this.Width = SystemParameters.PrimaryScreenWidth;
             this.Height = SystemParameters.PrimaryScreenHeight;
         }
-        private double CalculateAngle(Point center, Point target)
+        private static double CalculateAngle(Point center, Point target)
         {
             // 计算两点之间的差值
             double deltaX = target.X - center.X;
@@ -91,32 +100,33 @@ namespace SpringBoxIII
             double angleDegrees = angleRadians * (180 / Math.PI);
             return angleDegrees;
         }
-        private TimeSpan CalculatedDuration(double speed, double displacement)
+        private static TimeSpan CalculatedDuration(double speed, double displacement)
         {
             double totalSeconds = displacement / speed;
-            TimeSpan time = new(0, 0, 0, 0, 0);
-            time = TimeSpan.FromSeconds(totalSeconds);
+            if (totalSeconds < 0.1)
+            {
+                totalSeconds = 0.1;
+            }
+            TimeSpan time = TimeSpan.FromSeconds(totalSeconds);
+            Trace.WriteLine("time:" + time);
             return time;
         }
-        private void Timer_Tick(object? sender, EventArgs e)
+        private void PlayMoveAnimation(string animationName, Point To)
         {
-            Storyboard storyboard = (Storyboard)this.FindResource("MoveAnimation");
-            storyboard.Completed += (s, e) => { _isAnimationCompleted = true; };
-            Point imageCenter = new Point(
-                Img.ActualWidth / 2 + Canvas.GetLeft(Img),
-                Img.ActualHeight / 2 + Canvas.GetTop(Img)
-            );
+            ArgumentNullException.ThrowIfNull(animationName);
+
+            Img.Visibility = Visibility.Visible;
+            Trace.WriteLine("isAnimationCompleted:" + _isAnimationCompleted);
             if (DataContext is MainViewModel viewModel)
             {
-                Img.Visibility = Visibility.Visible;
-                if (_isAnimationCompleted == true)
+                Point imageCenter = new(Img.ActualWidth / 2 + Canvas.GetLeft(Img), Img.ActualHeight / 2 + Canvas.GetTop(Img));
+                viewModel.From = viewModel.To;
+                viewModel.To = To;
+                if (viewModel.To != viewModel.From)
                 {
-                    Random ran = new Random();
-                    viewModel.From = viewModel.To;
-                    viewModel.To = new(ran.Next(0, (int)this.ActualWidth) + 10, ran.Next(0, (int)this.ActualHeight) + 10);
                     int displacementX = (int)Math.Abs(viewModel.To.X - viewModel.From.X);
                     int displacementY = (int)Math.Abs(viewModel.To.Y - viewModel.From.Y);
-                    if (displacementX > displacementY)
+                    if (displacementX < displacementY)
                     {
                         viewModel.Duration = CalculatedDuration(_moveSpeed, (int)Math.Abs(viewModel.To.Y - viewModel.From.Y));
                     }
@@ -124,11 +134,68 @@ namespace SpringBoxIII
                     {
                         viewModel.Duration = CalculatedDuration(_moveSpeed, (int)Math.Abs(viewModel.To.X - viewModel.From.X));
                     }
-                    //Trace.WriteLine("Duration:" + viewModel.Duration);
                     viewModel.Angle = CalculateAngle(imageCenter, viewModel.To) - 90;
-                    Task.Delay(ran.Next(0, 3500)).Wait();
+                    Storyboard storyboard = (Storyboard)this.FindResource("MoveAnimation");
+                    storyboard.Completed += (s, e) => { _isAnimationCompleted = true; };
                     storyboard.Begin();
                     _isAnimationCompleted = false;
+                }
+            }
+        }
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (DataContext is MainViewModel viewModel)
+            {
+                if (_isEventCompleted && _isAnimationCompleted)
+                {
+                    // 产生随机事件
+                    List<int> randomEvents = [1, 2];
+                    List<int> weights = [5, 5];
+                    WeightedRandom weightedRandom = new(randomEvents, weights);
+                    randomEvent = weightedRandom.GetRandomValue();
+                    Trace.WriteLine("randomEvent:" + randomEvent);
+                }
+                if (randomEvent == 1)
+                {
+                    if (_isAnimationCompleted)
+                    {
+                        _moveSpeed = 350;
+                        Random ran = new(Guid.NewGuid().GetHashCode());
+                        Img.Visibility = Visibility.Visible;
+                        Task.Delay(ran.Next(0, 3500)).Wait();
+                        PlayMoveAnimation("MoveAnimation", new(ran.Next(0, (int)this.ActualWidth) + 10, ran.Next(0, (int)this.ActualHeight) + 10));
+                    }
+                }
+                else if (randomEvent == 2)
+                {
+                    _moveSpeed = 500;
+                    if (_isMovedToCursor)
+                    {
+                        Point windowPoint = new((int)Canvas.GetLeft(Img), (int)Canvas.GetTop(Img));
+                        var screenPoint = PointToScreen(windowPoint); // 转换为屏幕坐标
+                        SetCursorPos((int)screenPoint.X + 50, (int)screenPoint.Y + 50);
+                    }
+                    if (_isAnimationCompleted && !_isMovedToCursor)
+                    {
+                        Img.Visibility = Visibility.Visible;
+                        GetCursorPos(out System.Drawing.Point screenPoint);
+                        var windowPoint = PointFromScreen(new Point(screenPoint.X, screenPoint.Y)); // 转换为窗口坐标
+                        PlayMoveAnimation("MoveAnimation", windowPoint);
+                        if (Math.Abs(Canvas.GetLeft(Img) - windowPoint.X) <= 50 && Math.Abs(Canvas.GetTop(Img) - windowPoint.Y) <= 50)
+                        {
+                            _isMovedToCursor = true;
+                        }
+                    }
+                    if (_isAnimationCompleted && _isMovedToCursor)
+                    {
+                        Random ran = new(Guid.NewGuid().GetHashCode());
+                        PlayMoveAnimation("MoveAnimation", new(ran.Next(0, (int)this.ActualWidth) + 10, ran.Next(0, (int)this.ActualHeight) + 10));
+                        if (_isAnimationCompleted)
+                        {
+                            _isEventCompleted = true;
+                            _isMovedToCursor = false;
+                        }
+                    }
                 }
             }
         }
---
+++ SpringBoxIII/WeightedRandom.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpringBoxIII
+{
+    class WeightedRandom
+    {
+        private List<int> values;
+        private List<int> weights;
+        private int totalWeight;
+
+        // 构造函数，传入元素的值及对应的权重
+        public WeightedRandom(List<int> values, List<int> weights)
+        {
+            if (values.Count != weights.Count)
+                throw new ArgumentException("Values and weights must have the same length.");
+
+            this.values = values;
+            this.weights = weights;
+            totalWeight = 0;
+
+            // 计算所有权重的总和
+            foreach (var weight in weights)
+            {
+                totalWeight += weight;
+            }
+        }
+
+        // 获取加权随机元素
+        public int GetRandomValue()
+        {
+            Random random = new Random();
+            int randomWeight = random.Next(totalWeight);
+
+            int accumulatedWeight = 0;
+            for (int i = 0; i < values.Count; i++)
+            {
+                accumulatedWeight += weights[i];
+                if (randomWeight < accumulatedWeight)
+                {
+                    return values[i];
+                }
+            }
+            return -1;
+        }
+    }
+}
